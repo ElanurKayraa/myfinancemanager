ae71dc1b59cc227ed910de2e4435cca3350e521c
ElanurKayraa <elanur.guel@gmail.com>
2026-04-20 10:24:20 +0200

    Kommentare

--- src/main/java/de/htw_berlin/myfinancemanagerbackend/Ausgabe.java
+++ src/main/java/de/htw_berlin/myfinancemanagerbackend/Ausgabe.java
@@ -3,20 +3,20 @@ package de.htw_berlin.myfinancemanagerbackend;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
-@Entity
+@Entity //Entität, Datenbanktabelle
 public class Ausgabe {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //primärschlüssel
     private Long id;
-
+//Tabellenspalten
     private String titel;
     private double betrag;
     private String kategorie;
     private LocalDate datum;
-
+//leerer Konstrukto für Spring um Objekte zu erstellen
     public Ausgabe() {}
-
+//Konstruktor, speichert die Werte
     public Ausgabe(Long id, String titel, double betrag, String kategorie, LocalDate datum) {
         this.id = id;
         this.titel = titel;
@@ -25,7 +25,7 @@ public class Ausgabe {
         this.datum = datum;
     }
 
-    // Getters
+    // Getters zum auslesen
     public Long getId() { return id; }
     public String getTitel() { return titel; }
     public double getBetrag() { return betrag; }

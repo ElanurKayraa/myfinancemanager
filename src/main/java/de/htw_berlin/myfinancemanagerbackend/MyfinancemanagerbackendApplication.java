97437296974ab9991f92708832529b57d7844d71
ElanurKayraa <elanur.guel@gmail.com>
2026-04-20 10:30:16 +0200

    Add comments to MyfinancemanagerbackendApplication
    Added comments to clarify application startup and entry point.

--- src/main/java/de/htw_berlin/myfinancemanagerbackend/MyfinancemanagerbackendApplication.java
+++ src/main/java/de/htw_berlin/myfinancemanagerbackend/MyfinancemanagerbackendApplication.java
@@ -1,12 +1,12 @@
 package de.htw_berlin.myfinancemanagerbackend;
-
+//Startseite der App
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //kommmuniziert spring, dass hier die app startet
 public class MyfinancemanagerbackendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //Einstiegspunkt der App, ab hier läuft es auf localhost
 		SpringApplication.run(MyfinancemanagerbackendApplication.class, args);
 	}
 

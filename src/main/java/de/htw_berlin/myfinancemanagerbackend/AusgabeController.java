41e350d97986c7fcd57984b96baad2398254109e
ElanurKayraa <elanur.guel@gmail.com>
2026-04-20 10:28:29 +0200

    Kommenatre

--- src/main/java/de/htw_berlin/myfinancemanagerbackend/AusgabeController.java
+++ src/main/java/de/htw_berlin/myfinancemanagerbackend/AusgabeController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController
-@RequestMapping("/ausgaben")
+@RestController              //verbindet frontend und backend (HTTML -> JSON)
+@RequestMapping("/ausgaben") //alle Routen in diesre KLasse sind erreichbar unter /ausgaben
 public class AusgabeController {
 
-    @GetMapping
-    public List<Ausgabe> getAlle() {
-        return List.of(
+    @GetMapping                     //reagiertv auf Get Anfragen  (Browesraufrufe)
+    public List<Ausgabe> getAlle() { //Liste von Ausgabe-Objekten
+        return List.of(        //erstellt feste Liste mit 2 Beispeil Daten
                 new Ausgabe(1L, "Supermarkt", 42.50, "Lebensmittel", LocalDate.now()),
                 new Ausgabe(2L, "Netflix", 12.99, "Freizeit", LocalDate.now())
         );

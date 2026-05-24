c62ca6659601a3979c5e4c3823acd075af56e6be
ElanurKayraa <elanur.guel@gmail.com>
2026-05-24 19:19:31 +0200

    Add CrossOrigin annotation to AusgabeController

--- src/main/java/de/htw_berlin/myfinancemanagerbackend/AusgabeController.java
+++ src/main/java/de/htw_berlin/myfinancemanagerbackend/AusgabeController.java
@@ -1,16 +1,15 @@
 package de.htw_berlin.myfinancemanagerbackend;
-
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController              //verbindet frontend und backend (HTTML -> JSON)
-@RequestMapping("/ausgaben") //alle Routen in diesre KLasse sind erreichbar unter /ausgaben
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/ausgaben")
 public class AusgabeController {
-
-    @GetMapping                     //reagiertv auf Get Anfragen  (Browesraufrufe)
-    public List<Ausgabe> getAlle() { //Liste von Ausgabe-Objekten
-        return List.of(        //erstellt feste Liste mit 2 Beispeil Daten
+    @GetMapping
+    public List<Ausgabe> getAlle() {
+        return List.of(
                 new Ausgabe(1L, "Supermarkt", 42.50, "Lebensmittel", LocalDate.now()),
                 new Ausgabe(2L, "Netflix", 12.99, "Freizeit", LocalDate.now())
         );
